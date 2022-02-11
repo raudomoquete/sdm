d6dd38e93e2dc97518618cd08e9791f02645d18e
Raudo Moquete <raudo.moquete@rhodium.com.do>
2022-02-11 14:33:06 -0500

    skeleton upwork

--- Cars/src/app/app.module.ts
+++ Cars/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { VehicleListComponent } from './components/vehicles/vehicle-list/vehicle-list/vehicle-list.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { VehicleComponent } from './components/vehicles/vehicle/vehicle/vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiclesComponent,
+    VehicleListComponent,
+    FooterComponent,
+    HeaderComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
---
+++ Cars/src/app/models/vehicle.ts
@@ -0,0 +1,8 @@
+export class Vehicle {
+    year: number;
+    horsePower: number;
+    make: string;
+    model: string;
+    price: number;
+    img_url: string;
+}
